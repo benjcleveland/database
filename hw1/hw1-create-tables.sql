f5963319059170d913648d469f4c6c9da9000daf
Ben Cleveland <ben.j.cleveland@gmail.com>
2014-01-11 19:00:07 -0800

    added file

---
+++ hw1/hw1-create-tables.sql
@@ -0,0 +1,15 @@
+create table actor(id int primary key, fname text, lname text, gender char(1));
+copy actor from '/home/cleveb/imdb2010/actor-ascii.txt' with delimiter '|' csv quote E'\n';
+
+
+create table movie(id int primary key, name text, year int);
+copy movie from '/home/cleveb/imdb2010/movie-ascii.txt' with delimiter '|' csv quote E'\n';
+
+create table directors (id int primary key, fname text, lname text);
+copy directors from '/home/cleveb/imdb2010/directors-ascii.txt' with delimiter '|' csv quote E'\n';
+
+create table casts (pid int, mid int, foreign key(pid) references actor(id), foreign key(mid) references movie(id), role text);
+copy casts from '/home/cleveb/imdb2010/casts-ascii.txt' with delimiter '|' csv quote E'\n';
+
+create table movie_directors (did int, mid int, foreign key(did) references directors(id), foreign key(mid) references movie(id));
+copy movie_directors from '/home/cleveb/imdb2010/movie_directors-ascii.txt' with delimiter '|' csv quote E'\n';
