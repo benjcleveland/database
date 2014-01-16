7dd48c33f3ba7618660220a38c2066dad45b3cd4
Ben Cleveland <ben.j.cleveland@gmail.com>
2014-01-15 19:37:57 -0800

    another tweak

--- hw1/hw1-queries.sql
+++ hw1/hw1-queries.sql
@@ -42,6 +42,14 @@ where z.year = x.year
 9 - select w.name, w.c from (select max(z.c) as max_actors from (select m.id, m.name, count(distinct c.pid) as c from movie m, casts c where m.id = c.mid group by m.name, m.id) z) y, 
 (select m.id, m.name, count(distinct c.pid) as c from movie m, casts c where m.id = c.mid group by m.name, m.id) w
 where w.c = y.max_actors
+
+or 
+
+explain analyze select w.name, w.c from (select max(z.c) as max_actors from (select m.id, count(distinct c.pid) as c from movie m, casts c where m.id = c.mid group by  m.id) z) y, 
+(select m.id, m.name, count(distinct c.pid) as c from movie m, casts c where m.id = c.mid group by m.name, m.id) w
+where w.c = y.max_actors
+;
+
  
 
 10 - select z.year as start, (z.year + 10) as end, z.c from (select max(w.c) as max_movies from (select y.year, count(m.name) as c from (select distinct m.year from movie m) y, movie m where m.year < y.year + 10 and m.year >= y.year group by y.year) w) y, (select y.year, count(m.name) as c from (select distinct m.year from movie m) y, movie m where m.year < y.year + 10 and m.year >= y.year group by y.year) z where z.c = y.max_movies;
