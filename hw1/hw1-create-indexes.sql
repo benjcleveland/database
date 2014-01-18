c31ce9efe3c5023587af25806c135f297d854ab7
Ben Cleveland <ben.j.cleveland@gmail.com>
2014-01-18 12:02:04 -0800

    added the start of the indexes

---
+++ hw1/hw1-create-indexes.sql
@@ -0,0 +1,12 @@
+create unique index actor_idx on actor(id);
+
+create unique index movie_idx on movie(id);
+
+create unique index director_idx on director(id);
+
+create index pid_idx on casts(pid);
+
+create index mid_idx on casts(mid);
+
+create index year_idx on movie(year);
+
