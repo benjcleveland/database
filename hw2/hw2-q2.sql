ab54ade999995da41c3ae7ac22a036e528d73d3c
Ben Cleveland <ben.j.cleveland@gmail.com>
2014-01-25 10:53:24 -0800

    I think  I have all the tables...

--- hw2/hw2-q2.sql
+++ hw2/hw2-q2.sql
@@ -9,16 +9,17 @@
 /* create the vehicle table */
 create table vehicle(licencePlate text primary key, maxLiability int, maxLossDamage int,
     year int, ssn int, name text, foreign key(ssn) references person(ssn), 
-    foreign key(name) references insuranceco(name));
+    foreign key(name) references insuranceCo(name));
 
 /* create the truck table */
-create table truck(licencePlate text primary key, capacity int);
+create table truck(licencePlate text primary key, capacity int, ssn int, licenceNo text,
+    foreign key(ssn, licenceNo) references professionalDriver(ssn, licenceNo));
 
 /* create the car table */
 create table car(licencePlate text primary key, make text);
 
 /* create the InsuranceCo table */
-create table insuranceco(name text primary key, phone int);
+create table insuranceCo(name text primary key, phone int);
 
 /* create the person table */
 create table person(ssn int primary key, name text);
@@ -30,4 +31,10 @@ create table driver(ssn int, licenceNo text, primary key(ssn, licenceNo));
 create table nonProfessionalDriver(ssn int, licenceNo text, primary key(ssn, licenceNo));
 
 /* create the driver table */
+create table drives(ssn int. licenceNo text, licencePlate text, 
+    foreign key(ssn, licenceNo) references nonProfestionalDriver(ssn, licenceNo),
+    foreign key(licencePlate) references car(licencePlate));
 
+/* create the professional driver table */
+create table professionalDriver(ssn int, licenceNo text, medialHistory text, 
+    primary key(ssn, licenceNo));
