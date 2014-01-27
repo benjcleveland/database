259187472ca26ac7596a1811a0cf68fe029c2da6
Ben Cleveland <ben.j.cleveland@gmail.com>
2014-01-26 20:21:28 -0800

    updates

--- hw2/hw2-q5.sql
+++ hw2/hw2-q5.sql
@@ -1,39 +1,48 @@
+/*
+ Ben Cleveland
+ CSEP 544
+ Homework 2 Question 5
+*/
 
 /* create table */
-create table sales(name text, discount char(3), month char(3), price int);
+/* create table sales(name text, discount char(3), month char(3), price int); */
 
 /* import */
-\copy sales from '/home/cleveb/frumble/hw2-data.txt' with delimiter E'\t' csv header;
+/* \copy sales from '/home/cleveb/frumble/hw2-data.txt' with delimiter E'\t' csv header; */
 
 
-/* month->discount fd */
-select distinct(month, discount) from sales;
+/* Functional Dependency: month->discount */
+select month, discount from sales group by month, discount order by month;
 
-
-/* name->discount - no fd */
-
-/* name->price fd */
+/* Functional Dependency: name->price */
 /* todo make the return smaller... */
-select distinct(name, price) from sales;
+/* select distinct(name, price) from sales; */
+select price, array_agg(row(name)) as prod from (select name, price from sales group by name, price) as row group by price order by price;
 
 
+/* Other Functional Dependencies that I found that are not used in BCNF */
 /* name, discount, month -> price fd */
-select  count(distinct(price)), name, discount, month from sales group by name, discount, month order by name;
+/* select count(distinct(price)), name, discount, month from sales group by name, discount, month order by name; */
 
 /* name, month -> price fd */
-select name, month, count(distinct (price)) from sales group by name, month order by name;
+/* select name, month, count(distinct (price)) from sales group by name, month order by name; */
 
 /* name, month -> discount */
-select name, month, count(distinct (discount)) from sales group by name, month order by name;
+/* select name, month, count(distinct (discount)) from sales group by name, month order by name; */
 
 /* month price -> discount */
-select month, price, count(distinct (discount)) from sales group by month, price order by month;
+/* select month, price, count(distinct (discount)) from sales group by month, price order by month; */
 
 /* name discount - > price */
-select discount, name, count(distinct (price)) from sales group by discount, name order by name;
+/* select discount, name, count(distinct (price)) from sales group by discount, name order by name; */
 
 
+/* Decomposed schema: 
 
+    Name, Price
+    Month, Discount
+    Month, Name
+*/
 /* create the BCNF tables for the database */
 create table monthDiscount(month char(3) primary key, discount char(3));
 
@@ -44,9 +53,10 @@ create table product_months(month char(3), name text, primary key(name, month),
     foreign key(month) references monthDiscount(month));
 
 /* should we keep this table...? */
-create table monthprice(month char(3), price int, 
+/* create table monthprice(month char(3), price int, 
     primary key(month, price),
     foreign key(month) references monthDiscount(month));
+*/
 
 insert into product(name, price) select distinct name, price from sales;
 
@@ -54,10 +64,13 @@ insert into monthDiscount(month, discount) select distinct month, discount from 
 
 insert into product_months(name, month) select distinct name, month from sales;
 
+/*
 insert into monthprice(month, price) select distinct month, price from sales;
-
+*/
 select * from product;
 select * from monthDiscount;
 select * from product_months;
+/*
 select * from monthprice;
+*/
 
